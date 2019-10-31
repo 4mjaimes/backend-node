1ef79d2637c71e6affa53325cb7a77cd91df079e
Manuel Jaimes <4mjaimes@gmail.com>
2019-10-31 16:47:16 -0500

    API Rest

---
+++ movies-api/routes/movies.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { moviesMock } = require('../utils/mocks/movies');
+
+const moviesApi = (app) => {
+    const router = express.Router();
+    app.use('/api/movies/', router);
+    router.get('/', async (request, response, next) => {
+        try {
+            const movies = await Promise.resolve(moviesMock);
+            response.status(200).json({
+                data: movies,
+                message: 'Movies listed'
+            })
+        } catch (error) {
+            next(error);
+        }
+    });
+}
+
+export default moviesApi;
